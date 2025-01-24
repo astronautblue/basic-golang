b95d8b7936c579797406102e304b7285213e3056
di2t <aditya.saputra@brilife.co.id>
2025-01-24 14:15:14 +0700

    some improvement

---
+++ interface.go
@@ -0,0 +1,13 @@
+// package main
+
+// type Customers interface {
+// 	GetName() string
+// }
+
+// func sayHello(value HasName) {
+
+// }
+
+// func main() {
+
+// }
--- struct.go
+++ struct.go
@@ -1,37 +1,37 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-type Customers struct {
-	Name, Addres, NoHP string
-	Age                int
-}
+// type Customers struct {
+// 	Name, Addres, NoHP string
+// 	Age                int
+// }
 
-func (customer Customers) sayHello(name string) {
-	fmt.Println("Hallo saya " + name + " adalah " + customer.Name)
-}
+// func (customer Customers) sayHello(name string) {
+// 	fmt.Println("Hallo saya " + name + " adalah " + customer.Name)
+// }
 
-func main() {
-	// var customer Customers
+// func main() {
+// 	// var customer Customers
 
-	// customer.Name = "Aditya Saputra"
-	// customer.Addres = "Kota Bekasi"
-	// customer.NoHP = "0892828828"
-	// customer.Age = 30
+// 	// customer.Name = "Aditya Saputra"
+// 	// customer.Addres = "Kota Bekasi"
+// 	// customer.NoHP = "0892828828"
+// 	// customer.Age = 30
 
-	customer := Customers{
-		Name:   "Aditya",
-		Addres: "Bekasi",
-		NoHP:   "089292828",
-		Age:    20,
-	}
+// 	customer := Customers{
+// 		Name:   "Aditya",
+// 		Addres: "Bekasi",
+// 		NoHP:   "089292828",
+// 		Age:    20,
+// 	}
 
-	customer1 := Customers{"Aditya", "Bogor", "242424", 30}
+// 	customer1 := Customers{"Aditya", "Bogor", "242424", 30}
 
-	fmt.Println("Customers : ", customer.Name)
-	fmt.Println("Customers : ", customer1.Addres)
-	fmt.Println("Customers : ", customer.NoHP)
-	fmt.Println("Customers : ", customer.Age)
+// 	fmt.Println("Customers : ", customer.Name)
+// 	fmt.Println("Customers : ", customer1.Addres)
+// 	fmt.Println("Customers : ", customer.NoHP)
+// 	fmt.Println("Customers : ", customer.Age)
 
-	customer.sayHello("dodit")
-}
+// 	customer.sayHello("dodit")
+// }
